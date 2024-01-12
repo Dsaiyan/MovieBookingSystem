6179cbf885636f017eeb82c50f3fa10be5e4e61d
Sayan Deep <sayandeep24@gmail.com>
2024-01-12 19:37:23 +0530

    first commit

---
+++ src/main/java/com/scaler/practicebookmyshow/PracticeBookMyShowApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.practicebookmyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBookMyShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeBookMyShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/practicebookmyshow/PracticeBookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.practicebookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBookMyShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
